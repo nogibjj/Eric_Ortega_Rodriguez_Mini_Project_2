7d61d234f96ed86645896417bd2849556ee388b1
Eric Ortega <ericortega@Erics-MBP.lan>
2024-09-15 17:33:12 -0400

    updating file

--- main.py
+++ main.py
@@ -1,13 +1,46 @@
+"""
+        library file 
+"""
 
-def add(x, y):
-    return x + y
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = (
+    "https://raw.githubusercontent.com/fivethirtyeight/data/master/drug-use-by-age/drug-use-by-age.csv"
+)
 
 
-def main():
-    x = 1
-    y = 2
-    add(x, y)
+def load_dataset():
+    df = pd.read_csv(dataset)
+    return df
 
+def grab_mean(df, col):
+    return df[col].mean()
 
-if __name__ == "__main__":
-    main()
+def grab_median(df,col):
+    return df[col].median() 
+
+# def grab STD
+def grab_std(df,col):
+    return df[col].std()
+
+# def grab max
+def grab_max(df,col):
+    return df[col].max()
+
+def create_histogram(df , col):
+    df[col].plot.hist(bins=10, edgecolor ='black')
+    plt.title(col)
+
+df1 = load_dataset()
+df1.head()
+
+mean_alc = grab_mean(df1,"alcohol_use")
+
+median_alc = grab_median(df1,"alcohol_use")
+
+std_alc = grab_std(df1,"alcohol_use")
+
+max_alc = grab_max(df1,"alcohol_use")
+
+create_histogram(df1, "alcohol_use")
--- test.py
+++ test.py
@@ -1,19 +1,34 @@
 
-import unittest
-from main import add
+import pandas as pd
+import matplotlib.pyplot as plt
+from main import load_dataset,grab_mean,grab_median,grab_std,grab_max,create_histogram
 
-class TestAddFunction(unittest.TestCase):
-    def test_add_positive_numbers(self):
-        self.assertEqual(add(2, 3), 5)
+dataset = (
+    "https://raw.githubusercontent.com/fivethirtyeight/data/master/drug-use-by-age/drug-use-by-age.csv"
+)
 
-    def test_add_negative_numbers(self):
-        self.assertEqual(add(-2, -3), -5)
+df1 = load_dataset()
 
-    def test_add_mixed_numbers(self):
-        self.assertEqual(add(-2, 3), 1)
+def test_load_dataset():
+    assert len(df1)==17
 
-    def test_add_zero(self):
-        self.assertEqual(add(0, 0), 0)
+def test_grab_mean():
+    assert grab_mean(df1,"alcohol_use")<=56
+
+def test_grab_median():
+    assert grab_median(df1,"alcohol_use")<=65
+
+def test_grab_std():
+    assert grab_std(df1,"alcohol_use")<=27
+
+def test_grab_max():
+    assert grab_std(df1,"alcohol_use")<=85
+    assert grab_std(df1, "alcohol_use") >80
 
 if __name__ == '__main__':
-    unittest.main()
+    test_load_dataset()
+    test_grab_mean()
+    test_grab_median()
+    test_grab_std()
+    test_grab_max()
+
